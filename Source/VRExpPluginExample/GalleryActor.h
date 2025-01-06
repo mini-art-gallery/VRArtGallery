619a6f1321393a68d4489f6c4c1165e1c5564b38
Igor Faliszewski <74872004+faliszewskii@users.noreply.github.com>
2025-01-06 11:42:21 +0100

    add parent class for actor classes (#9)

--- Source/VRExpPluginExample/GalleryActor.h
+++ Source/VRExpPluginExample/GalleryActor.h
@@ -4,27 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Components/StaticMeshComponent.h"
-#include "GalleryActor.h"
-#include "GalleryModel.generated.h"
+#include "GalleryActor.generated.h"
 
 UCLASS()
-class VREXPPLUGINEXAMPLE_API AGalleryModel : public AGalleryActor
+class VREXPPLUGINEXAMPLE_API AGalleryActor : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AGalleryModel();
-
-	void SetMesh(UStaticMesh* mesh);
+	AGalleryActor();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UStaticMeshComponent* MeshComponent;
 
 public:	
 	// Called every frame
